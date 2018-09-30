4ff860efc95cb2e4c1383dfbfc989d4c19247968
Andreas Lorensen <andlo@fedora.lan>
2018-09-30 13:02:42 +0200

    ange a typo in reguire

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from mycroft import MycroftSkill, intent_handler
 
 class SayHelloTo(MycroftSkill):
     @intent_handler(IntentBuilder("") \
-            .requeire("to.hello.say.intent").require("Name"))
+            .require("to.hello.say.intent").require("Name"))
     def handle_say_hello_to_intent(self, message):
         name = message.data.get("Name")
         self.speak_dialog("to.hello.say", data=name)
