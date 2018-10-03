59a2148c04fb882dd4c77c27c0a1e7858d9538f9
Andreas Lorensen <andlo@outlook.dk>
2018-10-03 17:46:53 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -7,8 +7,7 @@ class SayHelloTo(MycroftSkill):
     @intent_handler(IntentBuilder("").require("say").optionally("hello").optionally("to"))
 
     def  handle_say_hello_to_intent(self, message):
-        #response = {'name': message.data.get("Name")}
-        response = {'name': message.utterance_remainder()}
+        response = {'name': message.data.get("Name")}
         #response = message.utterance_remainder()
         self.speak_dialog("to.hello.say", data=response)
 
