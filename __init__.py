bd6b13d1373a18b31f91ac6e88dd42ae40bee30d
Andreas Lorensen <andlo@outlook.dk>
2018-10-03 18:55:13 +0200

    IT IS WORKING :)

--- __init__.py
+++ __init__.py
@@ -2,7 +2,9 @@ from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
 
 class SayHelloTo(MycroftSkill):
-   
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        
     @intent_file_handler('to.hello.say.intent')
     def  handle_say_hello_to_intent(self, message):
         response = {'name': message.data.get("name")}
