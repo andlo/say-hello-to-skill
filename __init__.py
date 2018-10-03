a698bbee46db861c6e0bbe8f927facf5d34bf501
Andreas Lorensen <andlo@outlook.dk>
2018-10-03 17:11:08 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from mycroft import MycroftSkill, intent_handler
 
 class SayHelloTo(MycroftSkill):
    
-    @intent_handler(IntentBuilder("").require("say").optional("hello").optional("to"))
+    @intent_handler(IntentBuilder("").require("say").optionally("hello").optionally("to"))
 
     def  handle_say_hello_to_intent(self, message):
         response = {'name': message.data.get("Name")}
