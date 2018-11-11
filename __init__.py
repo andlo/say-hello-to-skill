9d4c131318594e301516c40ccf4ac0b3f9dc2ed3
Andreas Lorensen <andlo@outlook.dk>
2018-11-11 15:32:06 +0000

    codecleanup

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
-from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
+
 
 class SayHelloTo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('to.hello.say.intent')
-    def  handle_say_hello_to_intent(self, message):
+    def handle_say_hello_to_intent(self, message):
         response = {'name': message.data.get("name")}
         self.speak_dialog("to.hello.say", data=response)
+
 
 def create_skill():
     return SayHelloTo()
